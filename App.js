3d378518a9f865cf8bc1b23384ce6b7c4a6f0b44
Lechuguita <computersnew1@gmail.com>
2024-10-30 14:33:18 -0600

    second commit

--- App.js
+++ App.js
@@ -1,11 +1,8 @@
 import AppNavigation from './navigation/appNavigation';
-import { FavoritesProvider } from './providers/FavoritesContext';
 import "nativewind";
 
 export default function App() {
   return (
-    <FavoritesProvider>
       <AppNavigation />
-    </FavoritesProvider>
   );
 }
